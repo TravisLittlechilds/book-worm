a75a64a3b8170ea167aaff77edf8a8aa968b9aeb
Travis Littlechilds <travislittlechilds@gmail.com>
2016-01-10 21:06:59 +0000

    listing titles

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, url_for, render_template, request, redirect, flash
 from rauth import OAuth1Service, OAuth1Session
-import urlparse, logging
-import xml.etree.ElementTree as ET
-from logging.handlers import RotatingFileHandler
+from bs4 import BeautifulSoup
+import lxml
+import urlparse
 from tokens import *
 
 app = Flask(__name__, static_url_path='/static')
 
 # https://www.goodreads.com/api/oauth_example
+  
+def make_session():
+    
+    goodreads_session = OAuth1Session(
+    consumer_key = CONSUMER_KEY,
+    consumer_secret = CONSUMER_SECRET,
+    access_token = ACCESS_TOKEN,
+    access_token_secret = ACCESS_TOKEN_SECRET,
+    )
+    return goodreads_session
   
 # Flask Stuff
 
@@ -17,25 +27,18 @@ def main_page():
     
 
 @app.route('/add', methods=['POST'])    
-def addBook(): 
+def add_book(): 
 
     # don't know why but this needs to be re-made each request or it fails (404)
     # was in main as a global, but that caused it to only work once
-    goodreads_session = OAuth1Session(
-    consumer_key = CONSUMER_KEY,
-    consumer_secret = CONSUMER_SECRET,
-    access_token = ACCESS_TOKEN,
-    access_token_secret = ACCESS_TOKEN_SECRET,
-    )
+    
+    goodreads_session = make_session()
     
     # book_id 631932 is "The Greedy Python"
     data = {'name': 'to-read', 'book_id': 631932}
 
     # add this to our "to-read" shelf
     response = goodreads_session.post('http://www.goodreads.com/shelf/add_to_shelf.xml', data)
-    print '----worked----' + str(data)
-    print response.content
-    print '----worked---'
 
     if response.status_code != 201:
         flash('There was an error, it was ' + str(response.status_code))
@@ -46,20 +49,38 @@ def addBook():
         print 'Book added!'
     return redirect(url_for('main_page'))
     
-@app.route('/get_list', methods=['POST'])
+@app.route('/get_list', methods=['GET'])
 def get_list():
-    pass
-    return
+
+    goodreads_session = make_session()
+
+    data = {
+    'v': '2', 
+    'id': '4200483',
+    'shelf': 'to-read',
+    'per_page': '46'
+    }
+#
+    shelf_list = goodreads_session.get('http://www.goodreads.com/review/list.xml', params=data)
+
+    goodreads_soup = BeautifulSoup(shelf_list.content, "lxml")
     
-#@app.route('/get_userid', methods=['POST','GET'])
-#def getUserId():
+    title_list = goodreads_soup.findAll('title')
+    for each_title in title_list:
+        print each_title.string
+
+    return render_template('index.html', title_list=title_list )
+    
+
+@app.route('/get_userid', methods=['POST','GET'])
+def get_user_id():
 # my userid = 4200483
-#    response = goodreads_session.get('http://www.goodreads.com/api/auth_user')
-#    if response.status_code != 200:
-#       raise Exception('Cannot fetch resource: %s' % response.status_code)
-#    else:
-#        print 'this works2', response.content
-#    return render_template(url_for('main_page'))
+    response = goodreads_session.get('http://www.goodreads.com/api/auth_user')
+    if response.status_code != 200:
+       raise Exception('Cannot fetch resource: %s' % response.status_code)
+    else:
+        print 'this works2', response.content
+    return redirect(url_for('main_page'))
 
 
     
